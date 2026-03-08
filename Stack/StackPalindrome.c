ed89a11dcb87e6fa2f9e6c5ddba4180e985a319f
KyuHyun Kim <140415177+k2hop1213@users.noreply.github.com>
2026-03-08 16:59:44 +0900

    Update StackPalindrome.c

--- Stack/StackPalindrome.c
+++ Stack/StackPalindrome.c
@@ -21,14 +21,14 @@ int main() {
 	char buf[51];
 	scanf("%s", buf);
 
-	for (int i = 0; i < strlen(buf)/2; i++) {
+	for (int i = 0; i < strlen(buf)/2; i++) {//절반까지만 stack에 삽입
 		push(buf[i]);
 	}
-	int t = (strlen(buf) % 2 == 1) ? 1 : 0;
+	int t = (strlen(buf) % 2 == 1) ? 1 : 0;//문자열의 짝수 여부에 따라 1을 더할지 말지 정함(홀수면 한 칸 건너 뛰어야함)
 	for (int i = strlen(buf) / 2 + t; i < strlen(buf); i++) {
 		if (buf[i] == stack[top]) pop();
 		else {
-			printf("펠린드롬 아님");
+			printf("펠린드롬 아님");//나머지 절반에서 다른것이 나온다면 펠린드롬이 아님
 			return 0;
 		}
 	}
