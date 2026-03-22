e88f85f1fb8383aef73a41b761934001e2b5a2d9
KyuHyun Kim <140415177+k2hop1213@users.noreply.github.com>
2026-03-22 21:31:34 +0900

    Create recursion_fibo.c

---
+++ Recursion/recursion_fibo.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fibo(int n) {
+
+	if (n == 0)return 0;
+
+	if (n == 1)return 1;
+
+	
+	return fibo(n - 1) + fibo(n - 2);
+
+}
+
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d번째 피보나치 수 %d", n, fibo(n));
+
+	return 0;
+}
