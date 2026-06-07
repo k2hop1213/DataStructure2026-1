a7bd5206e194e23a0d045febe4725751ebe666a6
KyuHyun Kim <140415177+k2hop1213@users.noreply.github.com>
2026-06-07 19:04:08 +0900

    Update binary_search.c

--- Hashing/binary_search.c
+++ Hashing/binary_search.c
@@ -25,3 +25,12 @@ int binary_search_iter(int A[], int key, int low, int high) {
 	}
 	return -1;
 }
+
+int main()
+{
+    int	list[16] = { 8, 11, 12, 15, 16, 19, 20, 23, 25, 28, 29, 31, 33, 35, 38, 40 };
+    int key = 28;
+    printf("  %d 이진탐색(순환): %d\n", key, binary_search(list, key, 0, 15));
+    printf("\n");
+	return 0;
+}
