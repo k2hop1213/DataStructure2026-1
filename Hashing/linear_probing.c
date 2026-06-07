ccb9004e039b7a6a70cbfd5494ca96fa3a41cbdc
KyuHyun Kim <140415177+k2hop1213@users.noreply.github.com>
2026-06-07 15:48:47 +0900

    Create linear_probing.c

---
+++ Hashing/linear_probing.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define M 10000
+int table[M];
+int hashFn(int key) { return key % M; }
+int hashFn2(int key, int part) {
+	if (key <= 0 || part <= 0) return 0;
+
+	int len = 0;
+	int temp = key;
+	while (temp > 0) {
+		temp /= 10;
+		len++;
+	}
+
+	int size = (len + part - 1) / part; // 한 사이즈의 크기
+
+	int cut = 1;
+	for (int i = 0; i < size; i++) {
+		cut *= 10;
+	}
+
+	int res = 0;
+	while (key > 0) {
+		res += key % cut;  
+		key /= cut;        
+	}
+
+	return res;
+}
+
+int stringFolding(char* key) {
+	int hash = 0;
+	int len = strlen(key);
+
+	// 4글자씩 묶어서 하나의 정수로 변환 후 더하기
+	for (int i = 0; i < len; i += 4) {
+		int val = 0;
+		for (int j = 0; j < 4; j++) {
+			val <<= 8; // 8비트씩 왼쪽으로 밀어줍니다 (자릿수 이동)
+			if (i + j < len) {
+				val += key[i + j];
+			}
+		}
+		hash += val;
+	}
+	return hash;
+}
+void init_lp() {
+	for (int i = 0; i < M; i++)
+		table[i] = 0;
+}
+
+void insert_lp(int key) {
+	int i = hashFn(key);
+	for (int k = 0; k < M; k++) {
+		if (table[i] == 0 || table[i] == -1) {
+			table[i] = key;
+			return;
+		}
+		i = (i + 1) % M;
+	}
+}
+
+int search_lp(int key) {
+	int i = hashFn(key);
+	for (int k = 0; k < M; k++) {
+		if(table[i] == key) return i;
+		if (table[i] == 0) return -1;
+		i = (i + 1) % M;
+	}
+	return -1;
+}
+
+int delete_lp(int key) {
+	int i = hashFn(key);
+	for (int k = 0; k < M; k++) {
+		if (table[i] == key) {
+			table[i] = -1;
+			return i;
+		}
+		if (table[i] == 0)
+			return -1;
+		i = (i + 1) % M;
+	}
+	return -1;
+}
